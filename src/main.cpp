5ae82e97505094ad81b41113f6abed7bcd0cddce
ArturSekunda <a.sekunda2115@gmail.com>
2025-09-19 22:30:52 +0200

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,36 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+int main() {
+    // Create a Window and render it
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
+    // Creating a circleShape
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(350, 250);
+    sf::Clock clock;
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
+    while (window.isOpen()) {
+        sf::Event event;
+        // Event Handler
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
                 window.close();
-            }
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::F)) {
+
+            std::cout << "Clocking clock: " << clock.getElapsedTime().asSeconds() << "."  << clock.getElapsedTime().asMicroseconds() << "\n";
+
+
+        }else {
+            clock.restart();
         }
 
         window.clear();
+        window.draw(shape);
         window.display();
     }
+
+    return 0;
 }
