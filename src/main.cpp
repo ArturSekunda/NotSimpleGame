7f22570c880d4bd0ca41576dbf7e890e8a3f16ec
ArturSekunda <a.sekunda2115@gmail.com>
2025-09-20 20:24:58 +0200

    Added: Enemy, simple math and his movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,44 @@
-#include <any>
+#include <cmath>
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+float calculateDistance(const sf::Vector2f& pointA, const sf::Vector2f& pointB) {
+    float deltaX = pointB.x - pointA.x;
+    float deltaY = pointB.y - pointA.y;
+    return std::sqrt(deltaX * deltaX + deltaY * deltaY);
+}
+
 int main() {
+
+    bool DebugeMode = false;
+
     // Create a Window and render it
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
-    // Creating a circleShape
+    // Creating a circleShape (Player)
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
     shape.setPosition(350, 250);
+    shape.setOrigin(shape.getRadius(), shape.getRadius());
+
+    // Enemy
+    sf::CircleShape enemy(100.f);
+    enemy.setFillColor(sf::Color::Red);
+    enemy.setPosition(50, 50);
+    enemy.setOrigin(enemy.getRadius(), enemy.getRadius());
+
+    // Clock for delta time
     sf::Clock clock;
+
+    //Stats
+    auto playerHP = 100.f;
+    auto enemyHP = 100.f;
+
 
     window.setFramerateLimit(60);
     auto speed  =  200.f;
+    auto enemySpeed = 100.f;
 
-    while (window.isOpen()) {
+    while (window.isOpen() && !sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
         // Create an event
         sf::Event event;
         // Event Handler
@@ -26,19 +50,25 @@ int main() {
             }
         }
 
-        //Getting time
-
+        //DeltaTime
         auto deltaTime = clock.restart().asSeconds();
-        if (deltaTime > 0.033f) {
-            deltaTime = 0.033f;
+
+
+        sf::Vector2f shapePos = shape.getPosition();
+        sf::Vector2f enemyPos = enemy.getPosition();
+        float distance = calculateDistance(shapePos, enemyPos);
+        sf::Vector2f direction = shapePos - enemyPos;
+        enemy.move(direction * (enemySpeed / distance) * deltaTime);
+        std::cout << "Distance: " << distance << std::endl;
+        std::cout << "Direction: " << direction.x << ", " << direction.y << std::endl;
+
+        if (DebugeMode) {
+            std::cout << "Player: " << shapePos.x << ", " << shapePos.y << std::endl;
+            std::cout << "Enemy: " << enemyPos.x << ", " << enemyPos.y << std::endl;
         }
 
 
         // Check if key is pressed
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-            window.close();
-        }
-
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
             shape.move(-speed * deltaTime, 0.f);
 
@@ -54,17 +84,15 @@ int main() {
             shape.move(0.f, speed * deltaTime);
         }
 
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::F)) {
-
-                std::cout << "Clocking clock: " << clock.getElapsedTime().asSeconds() << "."  << clock.getElapsedTime().asMicroseconds() << "\n";
-
-
-            }else {
-                clock.restart();
-            }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) {
+            speed = 400.f;
+        }else {
+            speed = 200.f;
+        }
 
             window.clear();
             window.draw(shape);
+            window.draw(enemy);
             window.display();
         }
 
---
+++ src/math/darkMath.h
@@ -0,0 +1,14 @@
+//
+// Created by lukas on 20.09.2025.
+//
+
+#ifndef NOTSIMPLEGAME_DARKMATH_H
+#define NOTSIMPLEGAME_DARKMATH_H
+
+
+class darkMath {
+
+};
+
+
+#endif //NOTSIMPLEGAME_DARKMATH_H
